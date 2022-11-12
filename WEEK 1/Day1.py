c05ca865f1e3cc72332cd0eb06a1b176e2fb56e9
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-11-12 13:21:34 +0300

    day 1

---
+++ WEEK 1/Day1.py
@@ -0,0 +1,34 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(4%3)
+print(4/3)
+print(4**3)
+print(4//3)
+
+
+name='gabriel'
+family_name='riko'
+country='Kenya'
+print(name)
+print(family_name)
+print(country)
+print('I am enjoying 30 days of Python')
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Gabriel','Ngeti','Kenya']))
+print(type(name))
+print(type(True))
+print(type((12,23,12,12,2)))
+print(type({4,2,56}))
+print(type({'name':'gabriel','age':'22','course':'computer Science'}))
+
+import math
+eucludian_distance=math.dist([2],[3])
+eucludian_distance_2=math.dist([10],[8])
+print(eucludian_distance)
+print(eucludian_distance_2)
