6d1e19c20f1f8f6813db34c50d15bc15dd07315f
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-11-29 09:17:41 +0300

    math

---
+++ 30DaysOfPython/math.py
@@ -0,0 +1,14 @@
+from math import pi
+print(pi)
+
+from math import sqrt,pow,floor,ceil,log10
+print(pi)
+print(sqrt(2))
+print(pow(2,3))
+print(floor(9.81))
+print(ceil(9.81))
+print(log10(100))
+
+from random import random,randint
+print(random())
+print(randint(5,20))
