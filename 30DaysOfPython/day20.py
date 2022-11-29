6d7f3a816d280f439943cb2a18d781d7b6c2621a
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-11-29 18:57:07 +0300

    day20

--- 30DaysOfPython/day20.py
+++ 30DaysOfPython/day20.py
@@ -1,23 +1,41 @@
-import numpy
-lst=[1,2,3,4,5]
-np_arr=numpy.array(lst)
-print(np_arr+2)
+# import numpy
+# lst=[1,2,3,4,5]
+# np_arr=numpy.array(lst)
+# print(np_arr+2)
 
-import webbrowser # web browser module to open websites
+# import webbrowser # web browser module to open websites
 
-# list of urls: python
-# url_lists = [
-#     'http://www.python.org',
-#     'https://www.linkedin.com/in/asabeneh/',
-#     'https://github.com/Asabeneh',
-#     'https://twitter.com/Asabeneh',
-# ]
+# # list of urls: python
+# # url_lists = [
+# #     'http://www.python.org',
+# #     'https://www.linkedin.com/in/asabeneh/',
+# #     'https://github.com/Asabeneh',
+# #     'https://twitter.com/Asabeneh',
+# # ]
 
-# # opens the above list of websites in a different tab
-# for url in url_lists:
-#     webbrowser.open_new_tab(url)
+# # # opens the above list of websites in a different tab
+# # for url in url_lists:
+# #     webbrowser.open_new_tab(url)
+
+# import requests
+# import re
+# url='http://www.gutenberg.org/files/1112/1112.txt'
+# response=requests.get(url)
+# paragraph=response.text
+# new_paragraph=re.sub(pattern='[^\w\s]',repl='',string= paragraph)
+# new_paragraph_1=re.split(' ',new_paragraph)
+# wordset=set()
+# for i in new_paragraph_1:
+#     wordset.add(i)
+# final_result=[]
+# for each in wordset:
+#     value=new_paragraph.count(each)
+#     new_dict={value:each}
+#     final_result.append(new_dict)
+# for each in final_result:
+#     print(each)
 
 import requests
-url:'https://twitter.com/'
+url='https://api.thecatapi.com/v1/breeds'
 response=requests.get(url)
-print(response)
+paragraph=response.text
